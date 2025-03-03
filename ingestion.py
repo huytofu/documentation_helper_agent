71c7f4f1db370293140adcc9dd2aef92097887d7
huy.to <huy.to.ryan@gmail.com>
2025-03-03 15:35:46 +0700

    Modify Ingestion

--- ingestion.py
+++ ingestion.py
@@ -7,6 +7,14 @@ from graph.models.embeddings import embeddings
 load_dotenv()
 
 urls1 = [
+    
+]
+
+urls2 = [
+
+]
+
+urls3 = [
     "https://langchain-ai.github.io/langgraph/reference/graphs/",
     "https://langchain-ai.github.io/langgraph/reference/checkpointing/",
     "https://langchain-ai.github.io/langgraph/reference/store/",
@@ -27,11 +35,42 @@ urls1 = [
     "https://langchain-ai.github.io/langgraph/cloud/reference/environment_variables/"
 ]
 
-urls2 = [
+urls4 = [
 
 ]
 
-urls3 = [
+urls5 = [
+    "https://docs.copilotkit.ai/reference/sdk/python/RemoteEndpoints",
+    "https://docs.copilotkit.ai/reference/sdk/python/LangGraphAgent",
+    "https://docs.copilotkit.ai/reference/sdk/python/LangGraph",
+    "https://docs.copilotkit.ai/reference/sdk/python/CrewAIAgent",
+    "https://docs.copilotkit.ai/reference/sdk/python/CrewAI",
+    "https://docs.copilotkit.ai/coagents",
+    "https://docs.copilotkit.ai/coagents/quickstart/langgraph",
+    "https://docs.copilotkit.ai/coagents/agentic-chat-ui",
+    "https://docs.copilotkit.ai/coagents/generative-ui",
+    "https://docs.copilotkit.ai/coagents/generative-ui/agentic",
+    "https://docs.copilotkit.ai/coagents/generative-ui/tool-based",
+    "https://docs.copilotkit.ai/coagents/human-in-the-loop",
+    "https://docs.copilotkit.ai/coagents/human-in-the-loop/interrupt-flow",
+    "https://docs.copilotkit.ai/coagents/human-in-the-loop/node-flow",
+    "https://docs.copilotkit.ai/coagents/shared-state",
+    "https://docs.copilotkit.ai/coagents/shared-state/in-app-agent-read",
+    "https://docs.copilotkit.ai/coagents/shared-state/in-app-agent-write",
+    "https://docs.copilotkit.ai/coagents/shared-state/state-inputs-outputs",
+    "https://docs.copilotkit.ai/coagents/shared-state/predictive-state-updates",
+    "https://docs.copilotkit.ai/coagents/frontend-actions",
+    "https://docs.copilotkit.ai/coagents/multi-agent-flows",
+    "https://docs.copilotkit.ai/coagents/persistence/loading-agent-state",
+    "https://docs.copilotkit.ai/coagents/persistence/loading-message-history",
+    "https://docs.copilotkit.ai/coagents/persistence/message-persistence",
+    "https://docs.copilotkit.ai/coagents/advanced/adding-runtime-configuration",
+    "https://docs.copilotkit.ai/coagents/advanced/disabling-state-streaming",
+    "https://docs.copilotkit.ai/coagents/advanced/emit-messages",
+    "https://docs.copilotkit.ai/coagents/advanced/exit-agent"
+]
+
+urls6 = [
     "https://docs.copilotkit.ai/reference/components/chat/CopilotChat",
     "https://docs.copilotkit.ai/reference/components/chat/CopilotPopup",
     "https://docs.copilotkit.ai/reference/components/chat/CopilotSidebar",
@@ -53,21 +92,38 @@ urls3 = [
     "https://docs.copilotkit.ai/reference/classes/llm-adapters/LangChainAdapter",
     "https://docs.copilotkit.ai/reference/classes/llm-adapters/GroqAdapter",
     "https://docs.copilotkit.ai/reference/classes/llm-adapters/GoogleGenerativeAIAdapter",
-    "https://docs.copilotkit.ai/reference/sdk/python/RemoteEndpoints",
-    "https://docs.copilotkit.ai/reference/sdk/python/LangGraphAgent",
-    "https://docs.copilotkit.ai/reference/sdk/python/LangGraph",
     "https://docs.copilotkit.ai/reference/sdk/js/LangGraph",
-    "https://docs.copilotkit.ai/guides/custom-look-and-feel/customize-built-in-ui-components",
-    "https://docs.copilotkit.ai/guides/generative-ui",
-    "https://docs.copilotkit.ai/guides/bring-your-own-llm",
+    "https://docs.copilotkit.ai/coagents",
     "https://docs.copilotkit.ai/coagents/quickstart/langgraph",
+    "https://docs.copilotkit.ai/coagents/agentic-chat-ui",
+    "https://docs.copilotkit.ai/coagents/generative-ui",
+    "https://docs.copilotkit.ai/coagents/generative-ui/agentic",
+    "https://docs.copilotkit.ai/coagents/generative-ui/tool-based",
     "https://docs.copilotkit.ai/coagents/human-in-the-loop",
+    "https://docs.copilotkit.ai/coagents/human-in-the-loop/interrupt-flow",
+    "https://docs.copilotkit.ai/coagents/human-in-the-loop/node-flow",
     "https://docs.copilotkit.ai/coagents/shared-state",
+    "https://docs.copilotkit.ai/coagents/shared-state/in-app-agent-read",
+    "https://docs.copilotkit.ai/coagents/shared-state/in-app-agent-write",
+    "https://docs.copilotkit.ai/coagents/shared-state/state-inputs-outputs",
+    "https://docs.copilotkit.ai/coagents/shared-state/predictive-state-updates",
+    "https://docs.copilotkit.ai/coagents/frontend-actions",
     "https://docs.copilotkit.ai/coagents/multi-agent-flows",
-    "https://docs.copilotkit.ai/coagents"
+    "https://docs.copilotkit.ai/coagents/persistence/loading-agent-state",
+    "https://docs.copilotkit.ai/coagents/persistence/loading-message-history",
+    "https://docs.copilotkit.ai/coagents/persistence/message-persistence",
+    "https://docs.copilotkit.ai/coagents/advanced/adding-runtime-configuration",
+    "https://docs.copilotkit.ai/coagents/advanced/disabling-state-streaming",
+    "https://docs.copilotkit.ai/coagents/advanced/emit-messages",
+    "https://docs.copilotkit.ai/coagents/advanced/exit-agent"
 ]
 
-for framework, urls in zip([],[urls1, urls2, urls3]):
+for framework, urls in zip(
+        ["langchain_python", "langchain_javascript", 
+        "langgraph_python", "langgraph_javascript", 
+        "copilotkit_python", "copilotkit_javascript"], 
+        [urls1, urls2, urls3, urls4, urls5, urls6]
+    ):
     docs = [WebBaseLoader(url).load() for url in urls]
     docs_list = [item for sublist in docs for item in sublist]
 
