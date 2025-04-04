72488df7dfaeffe4ff67697676d6698050db8d53
huy.to <huy.to.ryan@gmail.com>
2025-04-04 17:25:40 +0700

    Sleep during post human in loop

--- agent/graph/nodes/post_human_in_loop.py
+++ agent/graph/nodes/post_human_in_loop.py
@@ -1,6 +1,8 @@
 from typing import Any, Dict
 from agent.graph.state import GraphState
+from time import sleep
 
 def post_human_in_loop(state: GraphState) -> Dict[str, Any]:
     print("---POST HUMAN IN LOOP---")
+    sleep(20)
     return {"current_node": "POST_HUMAN_IN_LOOP"}
