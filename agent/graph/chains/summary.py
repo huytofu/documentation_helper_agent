84f50238920e49203f23af616761896062d8a797
huy.to <huy.to.ryan@gmail.com>
2025-04-16 14:37:40 +0700

    Change summarize model back to llama

--- agent/graph/chains/summary.py
+++ agent/graph/chains/summary.py
@@ -4,17 +4,16 @@ from agent.graph.models.summarizer import llm
 from pydantic import BaseModel, Field
 
 class Summary(BaseModel):
-    """The rewritten version of the human user's last message that is more meaningful"""
-    rewritten_query: str = Field(description="The rewritten version of the human user's last message that is more meaningful")
+    """The rewritten version of the human user's last message"""
+    rewritten_query: str = Field(description="The rewritten version of the human user's last message")
 
 system = """you are given an input conversation between a human user and an AI assistant.
 Based on the conversation, rewrite the last message from the user into a more meaningful
-standalone query with better context while keeping the original meaning.
+& explicit query with better context while keeping the original meaning.
 
 RULES:
-1. Messages in the conversation that are more recent are more important.
-2. The rewritten query should be self-contained and clear and in English.
-3. The rewritten query should not have any quotes, prefixes, or explanations.
+1. The rewritten query should be self-contained, clear and in English.
+2. The rewritten query should not have any quotes, prefixes, or explanations.
 
 {format_instructions}
 
